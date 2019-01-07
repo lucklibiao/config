a6da34d5d84428e8949330599c744b17764f3ec1
libiao <libiao@xhqb.com>
2019-01-07 18:05:55 +0800

    add HandleFuncParse

--- Parse.go
+++ Parse.go
@@ -9,7 +9,9 @@ import (
 	"os"
 	"io/ioutil"
 	"path"
+	"encoding/json"
 	"fmt"
+
 	"gopkg.in/yaml.v2"
 )
 
@@ -18,17 +20,20 @@ type Bytes = []byte
 //将yaml 文件转为换struct
 
 type HandleFuncParse func(b Bytes, v interface{}) error
-func (p HandleFuncParse) StartParse(b Bytes, v interface{}){
-	p(b,v)
+func (p HandleFuncParse) StartParse(b Bytes, v interface{}) error {
+	return p(b,v)
 }
-
 
 
 func  Yaml(b Bytes, v interface{}) error{
 	return yaml.Unmarshal(b,v)
 }
 
+func  Json(b Bytes, v interface{}) error{
+	return json.Unmarshal(b,v)
+}
 
+////////////////////////////////////////////////////////////
 func Parse(file string,config interface{}) (err error){
 
 	//get the context  of file , bytes
@@ -40,18 +45,23 @@ func Parse(file string,config interface{}) (err error){
 
 	// check the file suffix ,if .json then go to jsonformat
 	// target: from json to struct
+
 	suffix:=path.Ext(fileWithDirectory)
 	switch suffix{
 	case ".yaml":
-		err=Yaml(b,config)
+		parser:=HandleFuncParse(Yaml)
+		return parser.StartParse(b,config)
+	case ".json":
+		parser:=HandleFuncParse(Json)
+		return parser.StartParse(b,config)
+	default:
+		return fmt.Errorf("unknown suffix: %s", suffix)
 	}
 	return err
 }
 
 // get the file context and return []byte
 func FileToBytes(s string) (Bytes, error) {
-	fmt.Println(s)
-	fmt.Println(ioutil.ReadFile(s))
 	return ioutil.ReadFile(s)
 }
 
